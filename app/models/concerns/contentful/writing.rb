10fa800c48ae6e737241b75eb7fbcc855c9c1439
Jonny Cavell <jonny.cavell@gmail.com>
2020-04-05 14:06:54 +0200

    Update writing list page with links and styling

--- app/models/concerns/contentful/writing.rb
+++ app/models/concerns/contentful/writing.rb
@@ -1,6 +1,9 @@
 module Contentful
   class Writing < ContentfulModel::Base
-
     self.content_type_id = 'writing'
+
+    def link
+      uri.include?('http') ? ['Read more (external link)', uri] : ['Read more', '/writing/' + uri]
+    end
   end
 end
